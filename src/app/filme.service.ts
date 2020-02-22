326a838898e4b6497f67d1fc21dee87005b7a4da
Guilherme Ribas Rodrigues da Silva <guilhermeribasrodrigues@gmail.com>
2020-02-21 22:11:04 -0300

    filme service utilizando HttpParams

--- src/app/filme.service.ts
+++ src/app/filme.service.ts
@@ -1,6 +1,6 @@
 import { environment } from './../environments/environment';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { map, tap } from 'rxjs/operators';
 
 @Injectable({
@@ -10,8 +10,14 @@ export class FilmeService {
 
   constructor(private http: HttpClient) { }
 
-  loadByName(filme){
-   return this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=${environment.API_KEY}&language=pt-PT&page=1&include_adult=false&query=${filme}`)
+  loadByName(filme: string) {
+    let params = new HttpParams();
+    params = params.set('api_key', environment.API_KEY);
+    params = params.set('language', 'pt-PT');
+    params = params.set('page', '1');
+    params = params.set('include_adult', 'false');
+    params = params.set('query', filme);
+    return this.http.get(`https://api.themoviedb.org/3/search/movie`, { params })
       .pipe(
         map((filmes: any) => filmes.results),
         tap(console.log),
